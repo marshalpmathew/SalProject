abc20b259d9c3af4ac15fa2799611f834fe37f5c
marshalpmathew <101464812+marshalpmathew@users.noreply.github.com>
2024-07-09 12:28:30 +0530

    Add files via upload

---
+++ pass-show-hide.js
@@ -0,0 +1,13 @@
+  const pswrdfield = document.querySelector(".form input[type='password']"),
+  toggleBtn = document.querySelector(".form .field i");
+
+  toggleBtn.onclick = ()=>{
+    if(pswrdfield.type == "password"){
+      pswrdfield.type = "text";
+      toggleBtn.classList.add("active");
+    }else{
+      pswrdfield.type = "password";
+      toggleBtn.classList.remove("active");
+    }
+  }
+  
